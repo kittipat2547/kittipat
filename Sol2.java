95c22b56c8d49c298d1f163a85c229a61afa59a3
kittipat2547 <158817008+kittipat2547@users.noreply.github.com>
2024-07-09 21:26:51 +0700

    too

---
+++ Sol2.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Sol2 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String A, B;
+
+        System.out.print("Get A:");
+        A = scan.nextLine();
+        System.out.print("Get B:");
+        B = scan.nextLine();
+
+        if (A.length() == B.length()) {
+            System.out.println("Same");
+        } else {
+            System.out.println("Not same");
+        }
+    }
+}
